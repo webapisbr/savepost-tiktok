3ddd82e4e2d0ccad8e1d674b5ddcbf1274198585
webapisbr <dev@webapis.com.br>
2023-09-20 12:04:50 -0300

    update

--- app.js
+++ app.js
@@ -5,9 +5,19 @@ const app = express();
 app.use(cors());
 
 app.get('/', async function(req, res){
-    const { TiktokDL } = require("@tobyg74/tiktok-api-dl");
+    const { TiktokDL, TiktokStalk } = require("@tobyg74/tiktok-api-dl");
     TiktokDL(req.url.replace('/?url=','')).then((result) => {
-        res.status(200).json(result);
+        if(result.status=='success'){
+            TiktokStalk(req.url.replace('/?url=','').replace('https://','').replace('http://','').split('/')[1].replace('@','')).then((result2) => {
+                if(result2.status=='success'){
+                    res.status(200).json({status: 'success', post: result.result, profile: result2.result});
+                }else{
+                    res.status(400).json({message: 'Falha na requisição', status: 'failed'});
+                }
+            });
+        }else{
+            res.status(400).json({message: 'Falha na requisição', status: 'failed'});
+        }
     }).catch(() => {
         res.status(400).json({message: 'Falha na requisição', status: 'failed'});
     });
