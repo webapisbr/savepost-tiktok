85beb2532c7f07f258c541bae3041ba02e65778e
webapisbr <dev@webapis.com.br>
2023-09-19 21:24:03 -0300

    Initial Commit

--- app.js
+++ app.js
@@ -5,17 +5,11 @@ const app = express();
 app.use(cors());
 
 app.get('/', async function(req, res){
-    const { TiktokDL } = require("@tobyg74/tiktok-api-dl");
-    TiktokDL(req.url.replace('/?url=','')).then((result) => {
-        console.log(result)
-        res.status(200).json(result);
-    }).catch(() => {
-        res.status(400).json({message: 'Falha na requisição', status: 'failed'});
-    });
+    res.status(400).json({message: 'Falha na requisição', status: 'failed'});
 });
 
 app.use(function(req, res) {
     res.status(404).json({message: 'Rota inválida', status: 'invalid'});
 });
 
-app.listen(precess.env.PORT || 3001, () => console.log(`Listening to port ${post}`));
+app.listen(3000, () => console.log(`Listening to port 3000`));
